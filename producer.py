ac38c5aeb820a84b892264cc815a2492a614f6ac
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-05-03 21:20:14 +0700

    feat: load mock json

--- producer.py
+++ producer.py
@@ -11,14 +11,18 @@ kafka_broker = os.environ["KAFKA_BROKER"]
 
 producer = KafkaProducer(bootstrap_servers=[kafka_broker])
 
-print("Running producer")
-for i in range(100):
-  content = json.dumps({
-    "index": i,
-    "text": random.choice(["A", "B", "C", "D"])
-  })
-  
-  print(f"Sending {content}")
-  producer.send("raw", content.encode("utf-8"))
+with open("example_json") as f:
+  data = json.load(f)
 
-  time.sleep(5)
+  print("Running producer")
+  for i in range(100):
+    # content = json.dumps({
+    #   "index": i,
+    #   "text": random.choice(["A", "B", "C", "D"])
+    # })
+    content = json.dumps(data)
+    
+    print(f"Sending {content}")
+    producer.send("raw", content.encode("utf-8"))
+
+    time.sleep(5)
